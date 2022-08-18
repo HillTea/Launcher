15a300e507ebf37001836fd8324104569dae430c
Your Name <you@example.com>
2022-08-18 15:05:47 +0200

    Mac & updates

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -51,7 +51,8 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('Nouvelle mise à jour disponible', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/HillTea/Launcher/releases/download/v${info.version}/Helios-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/HillTea/Launcher/releases/download/v${info.version}/holyages-installation.dmg`
+                    //info.darwindownload = `https://github.com/HillTea/Launcher/releases/download/v${info.version}/holyages-installation${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
                     showUpdateUI(info)
                 }
                 
