2616a2aec8f3efde917c5d9c187ef66a73126b9e
Your Name <you@example.com>
2022-08-21 05:04:38 +0200

    test

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -51,8 +51,8 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('Nouvelle mise à jour disponible', info.version)
                 
                 if(process.platform === 'darwin'){
-					info.darwindownload = `https://github.com/HillTea/Launcher/releases/download/v${info.version}/HolyAges.exe`
-                    //info.darwindownload = `https://github.com/dscalzi/HeliosLauncher/releases/download/v${info.version}/Helios-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/HillTea/Launcher/releases/download/v${info.version}/holyages-installation.dmg`
+                    //info.darwindownload = `https://github.com/HillTea/Launcher/releases/download/v${info.version}/holyages-installation${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
                     showUpdateUI(info)
                 }
                 
